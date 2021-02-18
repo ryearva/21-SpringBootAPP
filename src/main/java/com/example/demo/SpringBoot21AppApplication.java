c6b2d4781d6c6acb81570e902eff0bf55ff263bc
ryearva <rramanareddy28@gmail.com>
2021-02-19 03:16:54 +0530

    mai class modified

--- src/main/java/com/example/demo/SpringBoot21AppApplication.java
+++ src/main/java/com/example/demo/SpringBoot21AppApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBoot21AppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot21AppApplication.class, args);
+		ConfigurableApplicationContext run=SpringApplication.run(SpringBoot21AppApplication.class, args);
+		run.close();
 	}
 
 }
